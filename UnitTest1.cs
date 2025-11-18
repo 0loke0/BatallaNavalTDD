d0f7cff7c6daa10c39fce1c0ed6b755827a1b8dc
Jeison Fernandez <jeison.fernandez@sinco.com.co>
2025-11-18 11:11:07 -0500

    🔴 Si_SeAgregaUnJugador_Debe_AumentarElConteoDeJugadoresEnUno

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -27,22 +27,43 @@ public class BattleshipsTest
                                  " 9 |   |   |   |   |   |   |   |   |   |   |\n";
         tablero.Should().Be(tableroEsperado);
     }
+    
+    [Fact]
+    public void Si_SeAgregaUnJugador_Debe_AumentarElConteoDeJugadoresEnUno()
+    {
+        //Arrange
+        var batallaNaval = new BatallaNaval();
+        
+        //Act
+        batallaNaval.AddPlayer();
+        
+        //Assert
+        batallaNaval.cantidadJugadores.Should().Be(1);
+
+    }
 }
 
 public class BatallaNaval
 {
+    public int cantidadJugadores;
 
     public string Print()
     {
         return "   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |\n" +
-            " 0 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 1 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 2 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 3 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 4 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 5 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 6 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 7 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 8 |   |   |   |   |   |   |   |   |   |   |\n" +
-            " 9 |   |   |   |   |   |   |   |   |   |   |\n";
+               " 0 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 1 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 2 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 3 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 4 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 5 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 6 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 7 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 8 |   |   |   |   |   |   |   |   |   |   |\n" +
+               " 9 |   |   |   |   |   |   |   |   |   |   |\n";
     }
+
+    public void AddPlayer()
+    {
+        
+    }
+}
